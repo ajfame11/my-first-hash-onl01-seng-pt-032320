5096629aaf234a01ce4f0af7dffe4f758de3add3
aurel fontaine <ajfame11@gmail.com>
2020-04-02 03:36:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,10 @@
 def my_hash
-your_hash = {"food" => "yummy",   
+your_hash = {"food" => "yummy", "drink" => "thirsty"}
 end
 
 
 def shipping_manifest
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" =>
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
